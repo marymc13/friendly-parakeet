f290ddeb027fd39a392febf66c1dce3d4ad06af1
Mary Paul <marymc135@hotmail.com>
2022-06-27 22:30:01 -0400

    copied folder

---
+++ script.js
@@ -0,0 +1,28 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword () {
+  console.log("Button clicked.")
+
+  //1. Prompt the user for password criteria
+     //a. password length between 8 < 128    
+      //b. lowercase, uppercase, numbers, special characters
+  //2. Validate the input
+  //3.Generate password based on criteria
+  //4. Display generated password on the page
+  return "generated password will go here";
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
